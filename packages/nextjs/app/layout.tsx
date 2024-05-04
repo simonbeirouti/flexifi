d8d4330be0bfbfe05cf025e62ae7fa6713a57eec
simonbeirouti <hello@simonbeirouti.com>
2024-05-04 22:43:48 +1000

    tweak metadata

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -9,9 +9,9 @@ const baseUrl = process.env.VERCEL_URL
   : `http://localhost:${process.env.PORT || 3000}`;
 const imageUrl = `${baseUrl}/thumbnail.jpg`;
 
-const title = "Scaffold-ETH 2 App";
-const titleTemplate = "%s | Scaffold-ETH 2";
+const titleTemplate = "%s | FlexiFi";
 const description = "Built with 🏗 Scaffold-ETH 2";
+const title = "Home";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
